4951b1cbdc5d6c2a59932c50c38e11344892f33e
Martijn ter Schegget <54362214+mtersch-windesheim@users.noreply.github.com>
2021-04-13 10:11:13 +0200

    Initial commit

---
+++ src/nl/windesheim/bam24/demokbsm2/Main.java
@@ -0,0 +1,9 @@
+package nl.windesheim.bam24.demokbsm2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
